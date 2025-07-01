353eb648ad32bad1987a4eef3805eb4085546740
alexmowat <mowatalexei@gmail.com>
2025-07-01 15:40:06 -0600

    set up client, setup basic event listeners

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { Client, Events, GatewayIntentBits, Message } from 'discord.js'
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.once(Events.ClientReady, async (readyClient) => {
+    console.log("Client ready!")
+})
+
+client.on(Events.MessageCreate, async (message: Message) => {
+    console.log("received message: ", message.content)
+})
+
+const api_token = process.env.DISCORD_API_TOKEN
+
+if (!api_token) {
+    console.log("failed to get token")
+    process.exit(1)
+}
+
+await client.login(api_token)
